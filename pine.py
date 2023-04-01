7a07c7923dbdc1040e335f3eae7d80595667d1bc
Olivia Li <oliviali.ouyang@gmail.com>
2023-04-01 01:20:14 -0700

    auto generate pine scripts

---
+++ pine.py
@@ -0,0 +1,62 @@
+import openai
+import os
+import sys
+from dotenv import load_dotenv
+
+# Load the OpenAI API key from a .env file
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = api_key
+
+# Set the directory paths
+summarizations_path = "summarization"
+scripts_path = "scripts"
+
+
+def read_summary(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
+
+
+def generate_pine_script(summary):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages = [{"role": "system", "content" : "You are a Pine script generator for TradingView. You take a summary of a trading strategy and return a complete Pine script that implements that strategy. Be sure to implement the complete strategy that is given to you."},
+        {"role": "user", "content" : f'Create a Pine script for the following strategy:\n\n[{summary}]'}],
+        max_tokens=2000,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    script = response.choices[0].message.content.strip()
+    return script
+
+
+
+def save_script(script, file_name):
+    if not os.path.exists(scripts_path):
+        os.makedirs(scripts_path)
+
+    file_path = os.path.join(scripts_path, file_name)
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(script)
+    print(f"Script saved as '{file_name}'")
+
+
+def main():
+    if not os.path.exists(summarizations_path):
+        print(f"No summarizations directory found at '{summarizations_path}'")
+        sys.exit(1)
+
+    for file_name in os.listdir(summarizations_path):
+        if file_name.endswith(".txt"):
+            print(f"Generating script '{file_name}'...")
+            summarize_path = os.path.join(summarizations_path, file_name)
+            summary = read_summary(summarize_path)
+            script = generate_pine_script(summary)
+            save_script(script, file_name)
+
+
+if __name__ == "__main__":
+    main()
